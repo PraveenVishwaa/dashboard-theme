03069e7b442edbb38c66b99acb2f0e3e7b988419
unknown <praveen.viswanathan@ecosmob.com>
2021-10-08 12:08:58 +0530

    First commit

---
+++ src/app/custom-body/custom-body.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-custom-body',
+  templateUrl: './custom-body.component.html',
+  styleUrls: ['./custom-body.component.css']
+})
+export class CustomBodyComponent implements OnInit{
+
+  ngOnInit() {
+
+  }
+
+}
